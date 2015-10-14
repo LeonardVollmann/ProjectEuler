998c1fe185e60868def70c7f1945f9169000cd57
Leonard Vollmann <vollmann.leonard@gmail.com>
2015-10-14 22:38:49 +0200

    Problem #9

---
+++ 9.py
@@ -0,0 +1,12 @@
+def findTriplet(nums, target):
+	for i in range(0, len(nums)):
+		a = nums[i]
+		for j in range(i, len(nums)):
+			b = nums[j]
+			c = target - (a + b)
+			if a*a + b*b == c*c:
+				return a, b, c
+
+nums = [ x for x in range(1, 1000) ]
+result = findTriplet(nums, 1000)
+print(result[0] * result[1] * result[2])
