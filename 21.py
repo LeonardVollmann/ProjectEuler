1c3d8e99a3fe34b19f1b5ef77111abc7bc2c7c15
Leonard Vollmann <vollmann.leonard@gmail.com>
2017-09-10 17:55:15 +0200

    Problem #21

---
+++ 21.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+def d(n):
+	s = 0
+	for i in range(1, int(sqrt(n)) + 1):
+		if n % i == 0:
+			s += i + (n / i)
+
+	return s - n
+
+d = [d(n) for n in range(10000)]
+
+amicables = []
+for a in range(2, 10000):
+	b = d[a]
+	if b >= 10000:
+		continue
+
+	if d[b] == a and a != b:
+		amicables.append(a)
+		amicables.append(b)
+
+print sum(set(amicables))
